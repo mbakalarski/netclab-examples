1d7021c49983cd478bb9cd34e26937657256dc0e
mbakalarski <michal.bakalarski@gmail.com>
2025-01-30 15:40:04 +0100

    next commit

--- tools/apply_delete_topology.sh
+++ tools/apply_delete_topology.sh
@@ -3,8 +3,8 @@ BASE_DIR=$(git rev-parse --show-toplevel)
 TOPOLOGY_DIR="${BASE_DIR}/manifests/${2}"
 OPERATION="${1}"
 
-docker network ls | grep netclab && NETWORK="netclab"
-docker network ls | grep kind && NETWORK="kind"
+docker network ls | grep netclab 1>/dev/null && NETWORK="netclab"
+docker network ls | grep kind 1>/dev/null && NETWORK="kind"
 
 export HTTP_SERVER_IP=$(docker network inspect "${NETWORK}" | jq -r .[].IPAM.Config[].Gateway | grep -v ':')
 
