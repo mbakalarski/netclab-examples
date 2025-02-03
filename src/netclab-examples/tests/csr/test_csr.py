6365708b86ad37e270bdbde99840d50c6a07b0a5
mbakalarski <michal.bakalarski@gmail.com>
2025-02-03 17:10:26 +0100

    next commit

--- src/netclab-examples/tests/csr/test_csr.py
+++ src/netclab-examples/tests/csr/test_csr.py
@@ -1,13 +1,13 @@
-def test_before_configured_bgp(csr01):
+def test_before_configured_csr(csr01):
     out = csr01.cli.execute("show run | in router")
     assert "router bgp 6500" not in out
 
 
-def test_configured_bgp(configured_csr01):
+def test_configured_csr(configured_csr01):
     out = configured_csr01.cli.execute("show run | in router")
     assert "router bgp 6500" in out
 
 
-def test_after_configured_bgp(csr01):
+def test_after_configured_csr(csr01):
     out = csr01.cli.execute("show run | in router")
     assert "router bgp 6500" not in out
