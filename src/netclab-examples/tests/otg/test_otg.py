9952ab546e95d5685a4f9ecf435c517b3e92fd8c
mbakalarski <michal.bakalarski@gmail.com>
2025-02-03 17:37:26 +0100

    next commit

--- src/netclab-examples/tests/otg/test_otg.py
+++ src/netclab-examples/tests/otg/test_otg.py
@@ -1,20 +1,19 @@
 import pytest
 from snappi import Config
 
+pytestmark = pytest.mark.filterwarnings("ignore:.*Unverified HTTPS request.*:")
 
-@pytest.mark.filterwarnings("ignore:.*Unverified HTTPS request.*:")
+
 def test_before_configured_otg(otg01_api):
     config = otg01_api.get_config().serialize(encoding=Config.DICT)
     assert len(config) == 0
 
 
-@pytest.mark.filterwarnings("ignore:.*Unverified HTTPS request.*:")
 def test_configured_otg(configured_otg01_api):
     config = configured_otg01_api.get_config().serialize(encoding=Config.DICT)
     assert len(config) != 0
 
 
-@pytest.mark.filterwarnings("ignore:.*Unverified HTTPS request.*:")
 def test_after_configured_otg(otg01_api):
     config = otg01_api.get_config().serialize(encoding=Config.DICT)
     assert len(config) == 0
