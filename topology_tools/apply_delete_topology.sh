8c387a565de2ef3ab92c81d443e48859deb9a931
Michal B. <mbakalarski@acm.org>
2025-03-07 11:17:56 +0100

    http server ip in tools

--- topology_tools/apply_delete_topology.sh
+++ topology_tools/apply_delete_topology.sh
@@ -7,7 +7,11 @@ TOPOLOGY_DIR="${BASE_DIR}/manifests/${2}"
 OPERATION="${1}"
 
 docker network ls | grep kind 1>/dev/null && NETWORK="kind"
-export HTTP_SERVER_IP=$(docker network inspect "${NETWORK}" | jq -r .[].IPAM.Config[1].Gateway | grep -v ':')
+
+for gw in $(docker network inspect "${NETWORK}" | jq -r .[].IPAM.Config[].Gateway);
+do
+  echo $gw | grep -v ':' 1>/dev/null && export HTTP_SERVER_IP="${gw}" && break
+done
 
 if [ ${OPERATION} == "apply" ]; then
   kubectl kustomize "$TOPOLOGY_DIR" | envsubst '$HTTP_SERVER_IP' | kubectl "$OPERATION" -f -
